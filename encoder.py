468a6a514571baa185e861f00a96ca916025e517
mjavadhm <javadhmw@gmail.com>
2024-02-03 00:44:26 +0100

    creating encoder and decoder file

---
+++ encoder.py
@@ -0,0 +1,12 @@
+class Queue:
+    def __init__(self):
+        self.ants = []
+ 
+    def is_empty(self):
+        return self.ants == []
+ 
+    def enqueue(self, data):
+        self.ants.append(data)
+ 
+    def dequeue(self):
+        return self.ants.pop(0)
