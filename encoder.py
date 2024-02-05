36ea09d3394761c80228847dbf741a338b683ebb
mjavadhm <javadhmw@gmail.com>
2024-02-05 01:09:31 +0100

    checkpoint for making fshekast

--- encoder.py
+++ encoder.py
@@ -10,3 +10,31 @@ class Queue:
  
     def dequeue(self):
         return self.ants.pop(0)
+
+def isok(dna,queen,worker):
+    if dna[0]==queen[0]:
+        if kmp(dna=dna,worker=worker):
+            return True
+    else:
+        return False
+def kmp(dna, worker):
+    shekast = fshekast(dna)
+    i = 0
+    j = 0
+    
+    while i < len(worker):
+        return
+
+def fshekast(pat):
+    shekast = [-1] * len(pat)
+    #k = 0
+    j = 0
+
+    while j < len(pat):
+        k = 0
+        while k < j:
+            if pat[k] == 
+
+    
+    return shekast
+
