70c416b6d282cce0101c63910a1739832cc3188e
mjavadhm <javadhmw@gmail.com>
2024-02-05 02:40:34 +0100

    read_parents and red_input

--- encoder.py
+++ encoder.py
@@ -10,6 +10,8 @@ class Queue:
  
     def dequeue(self):
         return self.ants.pop(0)
+    def size(self):
+        return len(self.ants)
 
 def isok(dna,queen,worker):
     if dna[0]==queen[0]:
@@ -41,7 +43,40 @@ def fshekast(pat):
     while j < len(pat):
         k = 0
         while k < j:
-            if pat[k] == 
+            if pat[k] == pat[j]
 
     
     return shekast
+def read_parents():
+    queen = []
+    worker = []
+    
+    with open('parents.txt', 'r') as file:
+        lines = file.readlines()
+        
+        queens = int(lines[0].strip().split()[0])
+        workers = len(lines) - 1 - queens
+        
+        for i in range(1, queens + 1):
+            queen.append(lines[i].strip())
+        
+        for i in range(queens + 1, queens + workers + 1):
+            worker.append(lines[i].strip())
+
+    return queen, worker
+
+def read_input(queue):
+    with open('input.txt', 'r') as file:
+        lines = file.readlines()
+        
+        for line in lines:
+            dna = line.strip()
+            queue.enqueue(dna)
+
+def main():
+    queen, worker = read_parents()
+    queue = Queue()
+    read_input(queue)
+    tqueue = Queue()
+
+main()
